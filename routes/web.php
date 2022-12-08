ae4299f2bd48d8778978b51a444ea64baa23c09d
Awi Maulana <awimaulana19@gmail.com>
2022-12-08 21:41:24 +0800

    kembali

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AntrianController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PetugasController;
@@ -15,9 +16,8 @@ use App\Http\Controllers\PetugasController;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [AntrianController::class,'index']);
+Route::post('/', [AntrianController::class,'store']);
 
 Route::get('/login',[AuthController::class,'login']);
 Route::post('/login',[AuthController::class,'login_action']);
