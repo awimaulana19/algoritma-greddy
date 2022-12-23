4bd69debbf40f96434e96e4ee382d3c22889fadb
Awi Maulana <awimaulana19@gmail.com>
2022-12-23 19:18:18 +0800

    Pilihan Lihat Pass

--- resources/views/petugas/create.blade.php
+++ resources/views/petugas/create.blade.php
@@ -12,19 +12,21 @@
                         <div class="col-md-6">
                             <div class="mb-3">
                                 <label for="nama" class="form-label">Nama</label>
-                                <input class="form-control @error('nama') is-invalid @enderror" type="text" name="nama" placeholder="Masukan Nama">
+                                <input class="form-control @error('nama') is-invalid @enderror" type="text"
+                                    name="nama" placeholder="Masukkan Nama">
                                 @error('nama')
-                                <div class="invalid-feedback">
+                                    <div class="invalid-feedback">
                                         <i class="bi bi-exclamation-circle-fill"></i>
                                         {{ $message }}
                                     </div>
                                 @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="nama" class="form-label">Spesialis</label>
-                                <input class="form-control @error('spesialis') is-invalid @enderror" type="text" name="spesialis" placeholder="Masukan Spesialis">
+                                <label for="nama" class="form-label">Poli</label>
+                                <input class="form-control @error('spesialis') is-invalid @enderror" type="text"
+                                    name="spesialis" placeholder="Masukkan Spesialis">
                                 @error('spesialis')
-                                <div class="invalid-feedback">
+                                    <div class="invalid-feedback">
                                         <i class="bi bi-exclamation-circle-fill"></i>
                                         {{ $message }}
                                     </div>
@@ -32,9 +34,10 @@
                             </div>
                             <div class="mb-3">
                                 <label for="nama" class="form-label">Nomor HP</label>
-                                <input class="form-control  @error('nomor_hp') is-invalid @enderror" type="text" name="nomor_hp" placeholder="Masukan Nomor HP">
+                                <input class="form-control  @error('nomor_hp') is-invalid @enderror" type="text"
+                                    name="nomor_hp" placeholder="Masukkan Nomor HP">
                                 @error('nomor_hp')
-                                <div class="invalid-feedback">
+                                    <div class="invalid-feedback">
                                         <i class="bi bi-exclamation-circle-fill"></i>
                                         {{ $message }}
                                     </div>
@@ -44,29 +47,36 @@
                         <div class="col-md-6">
                             <div class="mb-3">
                                 <label for="username" class="form-label">Username</label>
-                                <input class="form-control  @error('username') is-invalid @enderror" type="text" name="username" placeholder="Masukan Username">
+                                <input class="form-control  @error('username') is-invalid @enderror" type="text"
+                                    name="username" placeholder="Masukkan Username">
                                 @error('username')
-                                <div class="invalid-feedback">
+                                    <div class="invalid-feedback">
                                         <i class="bi bi-exclamation-circle-fill"></i>
                                         {{ $message }}
                                     </div>
                                 @enderror
                             </div>
-                            <div class="mb-3">
+                            <div class="mb-3 form-password-toggle">
                                 <label for="password" class="form-label">Password</label>
-                                <input class="form-control  @error('password') is-invalid @enderror" type="password" name="password" placeholder="Masukan Password">
-                                @error('password')
-                                <div class="invalid-feedback">
-                                        <i class="bi bi-exclamation-circle-fill"></i>
-                                        {{ $message }}
-                                    </div>
-                                @enderror
+                                <div class="input-group input-group-merge">
+                                    <input class="form-control  @error('password') is-invalid @enderror" id="password"
+                                        type="password" name="password" placeholder="Masukkan Password"
+                                        aria-describedby="password">
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                    @error('password')
+                                        <div class="invalid-feedback">
+                                            <i class="bi bi-exclamation-circle-fill"></i>
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
                             </div>
                             <div class="mb-3">
                                 <label for="nama" class="form-label">Gambar</label>
-                                <input class="form-control  @error('gambar') is-invalid @enderror" type="file" id="gambar" name="gambar">
+                                <input class="form-control  @error('gambar') is-invalid @enderror" type="file"
+                                    id="gambar" name="gambar">
                                 @error('gambar')
-                                <div class="invalid-feedback">
+                                    <div class="invalid-feedback">
                                         <i class="bi bi-exclamation-circle-fill"></i>
                                         {{ $message }}
                                     </div>
